35080229b5234232522c6e22da2ae21e9492a537
Onur Dogan <onurinfo2@gmail.com>
2023-05-03 20:24:32 +0300

    #6 Introduciton Screens

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ogrenciden_canli_egitim_uygulamasi/constants/color_constants.dart';
 import 'package:ogrenciden_canli_egitim_uygulamasi/intro_screens/intro_screen_1.dart';
+import 'package:ogrenciden_canli_egitim_uygulamasi/intro_screens/intro_screen_2.dart';
 
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -16,6 +17,19 @@ class _PageViewDesignState extends State<PageViewDesign> {
 
   @override
   Widget build(BuildContext context) {
-    return Text("Deneme");
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            physics: const BouncingScrollPhysics(),
+            controller: _controller,
+            children: const <Widget>[IntroScreen(), IntroScreen2(), IntroScreen3()],
+          ),
+          Container(
+              alignment: const Alignment(0, 0.65),
+             )
+        ],
+      ),
+    );
   }
 }
