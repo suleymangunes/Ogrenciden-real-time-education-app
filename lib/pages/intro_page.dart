030f568fa27febfd3908bf12bf805b8c57dae569
Onur Dogan <onurinfo2@gmail.com>
2023-05-04 12:33:37 +0300

    #7 Introduciton Screens

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ogrenciden_canli_egitim_uygulamasi/constants/color_constants.dart';
 import 'package:ogrenciden_canli_egitim_uygulamasi/intro_screens/intro_screen_1.dart';
 import 'package:ogrenciden_canli_egitim_uygulamasi/intro_screens/intro_screen_2.dart';
-
+import 'package:ogrenciden_canli_egitim_uygulamasi/intro_screens/intro_screen_3.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class PageViewDesign extends StatefulWidget {
@@ -27,7 +27,22 @@ class _PageViewDesignState extends State<PageViewDesign> {
           ),
           Container(
               alignment: const Alignment(0, 0.65),
-             )
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SmoothPageIndicator(
+                          controller: _controller,
+                          count: 3,
+                          effect: ExpandingDotsEffect(
+                              activeDotColor: ColorConstants.instance.hippieGreen,
+                              dotColor: ColorConstants.instance.hippieGreen)),
+                    ],
+                  ),
+                ],
+              ))
         ],
       ),
     );
