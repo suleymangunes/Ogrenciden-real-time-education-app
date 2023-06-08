0204296a963dc905d10d9cc97338be7c8c860a0b
Onur Dogan <onurinfo2@gmail.com>
2023-06-08 21:58:24 +0300

    #72 Olusturdugum_derslerim Tasarım

---
+++ lib/pages/olusturdugum_derslerim.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ogrenciden_canli_egitim_uygulamasi/view/olusturulan_ders_card.dart';
+
+class OlusturdugumDerslerim extends StatefulWidget {
+  const OlusturdugumDerslerim({super.key, required this.liste});
+
+  final List liste;
+  @override
+  State<OlusturdugumDerslerim> createState() => _OlusturdugumDerslerimState();
+}
+
+class _OlusturdugumDerslerimState extends State<OlusturdugumDerslerim> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Oluşturduğum Dersler"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(
+              "assets/images/ogrenciden_logo_png.png",
+              height: Get.height * 0.05,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
